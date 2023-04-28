51622fa8385a5c0a4881e9ecc3549e3df4e5bbb2
Rodrigo Mello <rodrigojcmello@gmail.com>
2023-04-28 10:16:09 -0300

    first commit

---
+++ READEME.md
@@ -0,0 +1,9 @@
+# Description
+
+This ESLint configuration is the ultimate choice for developers who use ESLint, Airbnb Style Guide, React, React Hooks, Prettier, TypeScript, and Unicorn in their projects. It's designed to ensure adherence to best practices, proper code formatting, and high levels of code security and quality. With this configuration, you'll get the most optimal combination of features for improving code quality in React applications with TypeScript support, all while benefiting from the added security provided by the Unicorn rule set.
+
+## Installation
+
+```bash
+    npm install -D eslint-config-kiskadee @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-airbnb eslint-config-airbnb-base eslint-config-prettier eslint-plugin-import eslint-plugin-jest-dom eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-unicorn eslint-plugin-unused-imports prettier typescript
+```
---
+++ rules/react.js
@@ -0,0 +1,60 @@
+module.exports = {
+  /**
+   * The use of React.PropTypes is not recommended when working with TypeScript.
+   */
+  'react/require-default-props': 'off',
+
+  /**
+   * Establishes a consistent approach to declaring React components using
+   * arrow functions, as class components are no longer recommended.
+   */
+  'react/function-component-definition': [
+    'error',
+    {
+      namedComponents: 'arrow-function',
+      unnamedComponents: 'function-expression',
+    },
+  ],
+
+  /**
+   * This rule has been disabled because, starting from version 17,
+   * importing React is no longer mandatory.
+   * @see {@link https://reactjs.org/blog/2020/10/20/react-v17.html#new-jsx-transform}
+   */
+  'react/react-in-jsx-scope': 'off',
+
+  /**
+   * This configuration allows for a simpler way of changing a static string
+   * to a dynamic parameter by using braces instead of quotes. Instead of
+   * manually changing the quotes to braces when modifying a value, simply
+   * wrapping it in braces allows for easier and more efficient updates.
+   */
+  'react/jsx-curly-brace-presence': [
+    'error',
+    { props: 'always', children: 'never' },
+  ],
+
+  /**
+   * When a boolean property is set to true, it is more practical to keep
+   * the property name without the value. However, omitting the value can
+   * cause readability and maintenance issues for the component.
+   */
+  'react/jsx-boolean-value': ['error', 'always'],
+
+  /**
+   * With JavaScript, using the spread operator can create uncertainty
+   * regarding the contents of props, making it difficult to maintain
+   * readability. However, with TypeScript's strong typing system, this
+   * concern is reduced as the type of the variable is known, making it
+   * easier to ensure code clarity and maintainability.
+   * @see {@link https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-props-no-spreading.md}
+   * @see {@link https://www.typescriptlang.org/docs/handbook/interfaces.html}
+   */
+  'react/jsx-props-no-spreading': 'off',
+
+  /**
+   * Specifying all dependencies is not always required, and at times, this
+   * rule may cause more problems than it solves.
+   */
+  'react-hooks/exhaustive-deps': 'off',
+};
